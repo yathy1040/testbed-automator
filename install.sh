c90ebd58cc9821f1912ade117cc01eb16be24f9c
Yathusan Thulasinathan <32348509+yathy1040@users.noreply.github.com>
2026-03-06 15:22:40 -0500

    Fix issue with helm not installing
    Replaced Helm installation commands with a single script call.

--- install.sh
+++ install.sh
@@ -213,13 +213,7 @@ install-helm() {
     cecho "GREEN" "Helm 3 is not installed. Proceeding to install Helm ..."
 
     # Install Helm prerequisites
-    curl -s https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
-    sudo apt-get install apt-transport-https --yes
-
-    # Add Helm repository and install Helm
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-    sudo apt-get update
-    sudo apt-get install helm
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
   else
     cecho "YELLOW" "Helm 3 is already installed."
   fi
